872860213475d68ac2e6d69452334203be4130d1
Henok Getachew Alemayehu <henygetachew@gmail.com>
2016-10-21 17:49:01 +0300

    Add day of week support

--- medic-bot.js
+++ medic-bot.js
@@ -9,23 +9,40 @@ tasks.forEach(function(task){
   var scriptToRun = require(task.path)
   var scheduleText = `${min} minutes`;
   var hours
+  var days
 
   if(min > 60) {
     hours = min/60;
     scheduleText = `${hours} hours`;
   }
+
+  if(hours > 24){
+    days = hours/24;
+    if (days > 7){
+      throw 'Medic Bot does not support schedules of more than a week at the moment';
+    }
+
+    scheduleText = `${days} days`;
+  }
   
   if(task.active) {
     if(min > 60){
-      var sched = later.parse.recur().every(hours).hour(),
-        t = later.setInterval(scriptToRun, sched);
-      
-      console.log(`${task.name} Schedule set every ${hours} hours`);      
+      if(hours && hours > 24){
+        var sched = later.parse.recur().on(days).dayOfWeek();
+          t = later.setInterval(scriptToRun, sched);
+        
+        console.log(`${task.name} Schedule set every ${scheduleText}`);      
+      } else {
+        var sched = later.parse.recur().every(hours).hour(),
+          t = later.setInterval(scriptToRun, sched);
+        
+        console.log(`${task.name} Schedule set every ${scheduleText}`);      
+      }
     } else {
       var sched = later.parse.recur().every(min).minute(),
         t = later.setInterval(scriptToRun, sched);
       
-      console.log(`${task.name} Schedule set every ${min} minutes`);
+      console.log(`${task.name} Schedule set every ${scheduleText}`);
     }
 
     //notify_slack(`*Plugin Name*: ${task.name}\n*Description*: ${task.description} \n*Schedule*: Runs every ${scheduleText}.`);
